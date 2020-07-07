05f45c7b9adea1f44a250472daf9f10aaf0a31f7
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-07-07 16:44:58 -0300

    mapeamento da entidade Lancamento

--- src/main/java/com/guilherme/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/com/guilherme/minhasfinancas/model/entity/Lancamento.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "lancamento", schema = "financas")
@@ -14,6 +15,9 @@ public class Lancamento {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "descricao")
+    private Integer descricao;
 
     @Column(name = "mes")
     private Integer mes;
@@ -28,9 +32,101 @@ public class Lancamento {
     @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
     private LocalDate dataCadastro;
 
+    @Column(name = "tipo")
+    @Enumerated(value = EnumType.STRING )
+    private TipoLancamento tipoLancamento;
+
+    @Column(name = "status")
+    @Enumerated(value = EnumType.STRING )
+    private StatusLancamento statusLancamento;
 
 
     @ManyToOne
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(Integer descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getMes() {
+        return mes;
+    }
+
+    public void setMes(Integer mes) {
+        this.mes = mes;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public TipoLancamento getTipoLancamento() {
+        return tipoLancamento;
+    }
+
+    public void setTipoLancamento(TipoLancamento tipoLancamento) {
+        this.tipoLancamento = tipoLancamento;
+    }
+
+    public StatusLancamento getStatusLancamento() {
+        return statusLancamento;
+    }
+
+    public void setStatusLancamento(StatusLancamento statusLancamento) {
+        this.statusLancamento = statusLancamento;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lancamento)) return false;
+        Lancamento that = (Lancamento) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
