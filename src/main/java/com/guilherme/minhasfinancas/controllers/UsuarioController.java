c6bdaa8770831964851bde81a4c2a939ceae753c
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-07-08 14:53:49 -0300

    autenticacao de usuarios

--- src/main/java/com/guilherme/minhasfinancas/controllers/UsuarioController.java
+++ src/main/java/com/guilherme/minhasfinancas/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.guilherme.minhasfinancas.controllers;
 
+import com.guilherme.minhasfinancas.exception.ErroAutenticacao;
 import com.guilherme.minhasfinancas.exception.RegraNegocioException;
 import com.guilherme.minhasfinancas.model.dto.UsuarioDTO;
 import com.guilherme.minhasfinancas.model.entity.Usuario;
@@ -18,17 +19,30 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
     @PostMapping
-    public ResponseEntity salvar(@RequestBody UsuarioDTO dto){
+    public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+        try {
+            Usuario usuarioAutenticado = usuarioService.autenticar(dto.getEmail(),
+                    dto.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        } catch (ErroAutenticacao erroAutenticacao) {
+            return ResponseEntity.badRequest().body(erroAutenticacao.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
         Usuario usuario = Usuario.builder().nome(dto.getNome())
                 .email(dto.getEmail())
                 .senha(dto.getSenha())
                 .build();
-        try{
+        try {
             Usuario usuarioSalvo = usuarioService.salvarUsuario(usuario);
             return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
-        }catch (RegraNegocioException regraNegocioException){
+        } catch (RegraNegocioException regraNegocioException) {
             return ResponseEntity.badRequest().body(regraNegocioException.getMessage());
         }
     }
+
 }
