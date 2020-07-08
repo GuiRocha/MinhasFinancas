3cf267150420b3f041194bba9bd0e2825e8c2f9e
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-07-07 23:28:34 -0300

    criando mocks

--- src/test/java/com/guilherme/minhasfinancas/services/impl/UsuarioServiceImplTest.java
+++ src/test/java/com/guilherme/minhasfinancas/services/impl/UsuarioServiceImplTest.java
@@ -5,24 +5,42 @@ import com.guilherme.minhasfinancas.model.entity.Usuario;
 import com.guilherme.minhasfinancas.repositories.UsuarioRepository;
 import com.guilherme.minhasfinancas.services.UsuarioService;
 import org.junit.Test.None;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 class UsuarioServiceImplTest {
-    @Autowired
-    UsuarioService usuarioService;
 
-    @Autowired
+    UsuarioService usuarioService;
+    @MockBean
     UsuarioRepository usuarioRepository;
-    @Test
-    public void deveValidarEmail(){
-        usuarioRepository.deleteAll();
-        usuarioService.validarEmail("email@gmail.com");
+
+    @BeforeEach
+    public void setUp(){
+        usuarioService = new UsuarioServiceImpl(usuarioRepository);
     }
 
+    @Test
+    public void deveValidarEmail(){
+        Mockito.when(usuarioRepository
+                .existsByEmail(Mockito.anyString()))
+                .thenReturn(false);
+        usuarioService.validarEmail("email@gmail.com");
+    }
+    @Test
+    void deveLancarErroAoValidarEmailsQuandoExistirCadastro(){
+        try {
+            Mockito.when(usuarioRepository
+                    .existsByEmail(Mockito.anyString()))
+                    .thenReturn(true);
+        }catch (RegraNegocioException regraNegocioException){
+            usuarioService.validarEmail("email@gmail.com");
+        }
+    }
 }
